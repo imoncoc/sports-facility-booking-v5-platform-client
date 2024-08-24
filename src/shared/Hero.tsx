0dc095721dc9e98516141a2e619b4b83b293a52a
Md Imon Hossain <imoncoc0@gmail.com>
2024-08-25 02:11:07 +0600

    added dark mode, redux

--- src/shared/Hero.tsx
+++ src/shared/Hero.tsx
@@ -1,13 +1,15 @@
 const Hero = () => {
   return (
-    <div className="container mx-auto">
-      <h1>Hello, Hero!</h1>
-      <button className="p-4 px-8 rounded-full shadow-lg bg-strongCyan duration-200 hover:opacity-80 text-white">
-        Download
-      </button>
-      <button className="p-4 px-8 rounded-full shadow-lg bg-lightBlue duration-200 hover:opacity-80 text-white">
-        Down Ignore
-      </button>
+    <div className="dark:bg-slate-800 dark:text-white">
+      <div className="container mx-auto ">
+        <h1>Hello, Hero!</h1>
+        <button className="p-4 px-8 rounded-full shadow-lg bg-strongCyan duration-200 hover:opacity-80 text-white">
+          Download
+        </button>
+        <button className="p-4 px-8 rounded-full shadow-lg bg-lightBlue duration-200 hover:opacity-80 text-white">
+          Down Ignore
+        </button>
+      </div>
     </div>
   );
 };
--- src/shared/Navbar.tsx
+++ src/shared/Navbar.tsx
@@ -1,27 +1,99 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+import { MoonOutlined, SunOutlined } from "@ant-design/icons";
+import { Select, Tooltip } from "antd";
 
 const Navbar = () => {
   const [menuButton, setMenuButton] = useState(false);
+  const [theme, setTheme] = useState(false);
+
+  console.log({ theme });
+
+  useEffect(() => {
+    if (theme) {
+      document.documentElement.classList.add("dark");
+      document.documentElement.classList.remove("light");
+    } else {
+      document.documentElement.classList.add("light");
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(!theme);
+  };
+
+  const handleChange = (value: string) => {
+    if (value === "Dark") {
+      setTheme(true);
+    } else {
+      setTheme(false);
+    }
+    console.log(`selected ${value}`);
+  };
+
+  const options = [
+    {
+      value: "Light",
+      label: (
+        <span>
+          <SunOutlined className="text-slate-500 bg-white  " />{" "}
+          <span className="me-2">Light Mode</span>
+        </span>
+      ),
+    },
+    {
+      value: "Dark",
+      label: (
+        <span>
+          <MoonOutlined className="text-slate-500 bg-white  " />{" "}
+          <span className="me-2">Dark Mode</span>
+        </span>
+      ),
+    },
+  ];
 
   return (
-    <div>
-      <nav className="relative container mx-auto p-6 dark:bg-veryDarkBlue">
+    <div className="dark:bg-veryDarkBlue">
+      <nav className="relative container mx-auto p-6 ">
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-20">
             {/* <!-- Logo --> */}
             {/* <img src="./images/logo.svg" alt=""> */}
             {/* <!-- Left Menu --> */}
-            <div className="hidden space-x-8 font-bold lg:flex">
-              <a href="#" className="text-grayishViolet hover:text-strongCyan">
-                Features
-              </a>
-              <a href="#" className="text-grayishViolet hover:text-strongCyan">
-                Pricing
-              </a>
-              <a href="#" className="text-grayishViolet hover:text-strongCyan">
-                Resources
-              </a>
+            <div className="hidden text-lg uppercase space-x-8 font-bold lg:flex">
+              <NavLink
+                to="/about-us"
+                className="text-grayishViolet hover:text-strongCyan"
+              >
+                About Us
+              </NavLink>
+              <NavLink
+                to="/contact-us"
+                className="text-grayishViolet hover:text-strongCyan"
+              >
+                Contact us
+              </NavLink>
+              <NavLink
+                to="/facility-listing"
+                className="text-grayishViolet hover:text-strongCyan"
+              >
+                Facility Listing
+              </NavLink>
+              <NavLink
+                to="/booking"
+                className="text-grayishViolet hover:text-strongCyan"
+              >
+                Booking
+              </NavLink>
+              <NavLink
+                to="#"
+                className="text-grayishViolet hover:text-strongCyan"
+                onClick={handleThemeSwitch}
+              >
+                DarkMode
+              </NavLink>
             </div>
           </div>
 
@@ -34,6 +106,23 @@ const Navbar = () => {
             >
               Sign Up
             </a>
+            <div className="">
+              {theme ? (
+                <Tooltip placement="top" title={"Light"}>
+                  <SunOutlined
+                    className="text-slate-500 bg-white text-2xl p-2 cursor-pointer rounded-full shadow-md"
+                    onClick={() => setTheme(false)}
+                  />
+                </Tooltip>
+              ) : (
+                <Tooltip placement="top" title={"Dark"}>
+                  <MoonOutlined
+                    className="text-slate-200 bg-slate-800 text-2xl p-2 rounded-full cursor-pointer shadow-md"
+                    onClick={() => setTheme(true)}
+                  />
+                </Tooltip>
+              )}
+            </div>
           </div>
 
           {/* <!--  Hamburger Menu --> */}
@@ -59,15 +148,21 @@ const Navbar = () => {
           }`}
         >
           <div className="flex flex-col items-center justify-center w-full space-y-6 font-bold text-white rounded-sm">
-            <a href="#" className="w-full text-center">
-              Features
-            </a>
-            <a href="#" className="w-full text-center">
-              Pricing
-            </a>
-            <a href="#" className="w-full text-center">
-              Resources
-            </a>
+            <NavLink to="/about-us" className="w-full text-center uppercase">
+              About us
+            </NavLink>
+            <NavLink to="/contact-us" className="w-full text-center uppercase">
+              Contact us
+            </NavLink>
+            <NavLink
+              to="/facility-listing"
+              className="w-full text-center uppercase"
+            >
+              Facility Listing
+            </NavLink>
+            <NavLink to="/booking" className="w-full text-center uppercase">
+              Booking
+            </NavLink>
             <a
               href="#"
               className="w-full text-center pt-6 border-t border-gray-400"
@@ -80,6 +175,14 @@ const Navbar = () => {
             >
               Sign Up
             </a>
+
+            <Select
+              className="w-full"
+              defaultValue="lucy"
+              style={{ width: "100%" }}
+              onChange={handleChange}
+              options={options}
+            />
           </div>
         </div>
       </nav>
