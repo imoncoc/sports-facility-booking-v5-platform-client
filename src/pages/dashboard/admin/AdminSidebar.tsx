46c7dc13f44cb663f1ec31a196c630fad97b1403
Md Imon Hossain <imoncoc0@gmail.com>
2024-08-30 19:34:43 +0600

    type error

--- src/pages/dashboard/admin/AdminSidebar.tsx
+++ src/pages/dashboard/admin/AdminSidebar.tsx
@@ -1,13 +1,46 @@
 import { Flex, Menu } from "antd";
 import runningIcon from "../../../assets/running.png";
 import { UserOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const AdminSidebar = () => {
+  const navigate = useNavigate();
+  const [selectedKey, setSelectedKey] = useState("");
   const itemList = [
-    { key: "1", icon: <UserOutlined />, label: "Dashboard" },
-    { key: "2", icon: <UserOutlined />, label: "Admin 2" },
-    { key: "3", icon: <UserOutlined />, label: "Admin 3" },
+    {
+      key: "1",
+      icon: <UserOutlined />,
+      label: "Dashboard",
+      route: "/admin/dashboard",
+    },
+    {
+      key: "2",
+      icon: <UserOutlined />,
+      label: "Facility Management",
+      route: "/admin/dashboard/facility-management",
+    },
+    {
+      key: "3",
+      icon: <UserOutlined />,
+      label: "Bookings",
+      route: "/admin/dashboard/booking",
+    },
+    {
+      key: "4",
+      icon: <UserOutlined />,
+      label: "Create Admin",
+      route: "/admin/dashboard/create-account",
+    },
   ];
+
+  const onClick = (e: any) => {
+    const selectedItem = itemList.find((item) => item.key === e.key);
+    if (selectedItem && selectedItem.route) {
+      setSelectedKey(e.key); // Update the selected key state
+      navigate(selectedItem.route);
+    }
+  };
   return (
     <>
       <Flex align="center" justify="center" className="">
@@ -18,8 +51,13 @@ const AdminSidebar = () => {
 
       <Menu
         mode="inline"
-        items={itemList}
-        defaultSelectedKeys={["1"]}
+        onClick={onClick}
+        items={itemList.map((item) => ({
+          key: item.key,
+          icon: item.icon,
+          label: item.label,
+        }))}
+        selectedKeys={[selectedKey]}
         className="menu-bar"
       />
     </>
